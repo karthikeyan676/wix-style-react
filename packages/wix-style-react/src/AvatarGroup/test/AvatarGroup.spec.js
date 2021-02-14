9c779d7e179f5428a95d846c90f128305b6a1c2c
roibWix <roib@wix.com>
2021-02-14 15:26:28 +0200

    move items to util

--- packages/wix-style-react/src/AvatarGroup/test/AvatarGroup.spec.js
+++ packages/wix-style-react/src/AvatarGroup/test/AvatarGroup.spec.js
@@ -3,55 +3,7 @@ import {createRendererWithUniDriver, cleanup} from '../../../test/utils/unit';
 
 import AvatarGroup from '../AvatarGroup';
 import {avatarGroupPrivateDriverFactory} from './AvatarGroup.private.uni.driver';
-
-const avatarItems = [
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    color: 'A1',
-    imgProps: undefined,
-    name: 'Roi Bendet',
-    placeholder: undefined,
-    text: undefined,
-    title: 'Wix Account: John Doe (johndoe@gmail.com)',
-  },
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    name: 'Roi Bendet',
-    text: 'boom boom',
-    title: 'Wix Account: John Doe (johndoe@gmail.com)',
-  },
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    imgProps: undefined,
-    name: 'Roi Bendet',
-  },
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    text: 'hello world',
-    title: 'Wix Account: John Doe (johndoe@gmail.com)',
-  },
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    name: 'Tal Zaidman',
-    placeholder: undefined,
-    text: undefined,
-    title: 'Wix Account: John Doe (johndoe@gmail.com)',
-  },
-  {
-    name: 'Eran Egozi',
-    title: 'Wix Account: John Doe (johndoe@gmail.com)',
-  },
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    name: 'Ofir Bendet',
-    title: 'Wix Account: John Doe (johndoe@gmail.com)',
-  },
-  {
-    ariaLabel: 'Avatar for Roi Bendet',
-    imgProps: undefined,
-    name: 'Omer Bendet',
-  },
-];
+import { avatarItems } from '../utils';
 
 describe(AvatarGroup.displayName, () => {
   const render = createRendererWithUniDriver(avatarGroupPrivateDriverFactory);
@@ -96,16 +48,8 @@ describe(AvatarGroup.displayName, () => {
   });
 
   it('should return the text content of the avatar', async () => {
-    const AvatarGroupItems = [
-      {
-        name: 'Roi Bendet',
-      },
-      {
-        name: 'Eran Egozi',
-      },
-    ];
-    const expectedTextContent = 'EE';
-    const {driver} = render(<AvatarGroup items={AvatarGroupItems}/>);
+    const expectedTextContent = 'SD';
+    const {driver} = render(<AvatarGroup items={avatarItems}/>);
     expect(await driver.getAvatarTextContentByIndex(1)).toBe(expectedTextContent);
   });
 });
--- packages/wix-style-react/src/AvatarGroup/utils.js
+++ packages/wix-style-react/src/AvatarGroup/utils.js
@@ -85,3 +85,46 @@ export const limitItemsLength = (
   }
   return items;
 };
+
+export const avatarItems = [
+  {
+    ariaLabel: 'Avatar for John Doe',
+    color: 'A1',
+    name: 'John Doe',
+    title: 'Wix Account: John Doe (johndoe@gmail.com)',
+  },
+  {
+    ariaLabel: 'Avatar for Snoop Dogg',
+    name: 'Snoop Dogg',
+    title: 'Wix Account: Snoop Dogg (dog@gmail.com)',
+  },
+  {
+    ariaLabel: 'Avatar for Sia Kate',
+    imgProps: {
+      src: 'https://lh3.googleusercontent.com/proxy/EI3faU3-0hCPyjBKktiVuPKfZTMXx6BsDFE0f7UBr_8CKSMRWL28dLZJrWGWExcTRgWI2v3pcuBT19YpJ-xdmxla4mmqWw'
+    },
+    name: 'Sia Kate',
+  },
+  {
+    ariaLabel: 'Avatar for Steven Tyler',
+    title: 'Wix Account: Steven Tyler (steventyler@gmail.com)',
+  },
+  {
+    ariaLabel: 'Avatar for Karen Carpenter',
+    name: 'Karen Carpenter',
+    title: 'Wix Account: Karen Carpenter (karencarpenter@gmail.com)',
+  },
+  {
+    name: 'Levon Helm',
+    title: 'Wix Account: Levon Helm (levonhelm@gmail.com)',
+  },
+  {
+    ariaLabel: 'Avatar for Willie Nelson',
+    name: 'Willie Nelson',
+    title: 'Wix Account: Willie Nelson (willienelson@gmail.com)',
+  },
+  {
+    ariaLabel: 'Avatar for Patti Smith',
+    name: 'Patti Smith',
+  },
+];
