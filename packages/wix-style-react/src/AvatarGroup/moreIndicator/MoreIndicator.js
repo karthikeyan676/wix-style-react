427667239175fd159b25a543a3976b7dbb418ef4
roibWix <roib@wix.com>
2021-02-14 12:03:46 +0200

    cleanup code

--- packages/wix-style-react/src/AvatarGroup/moreIndicator/MoreIndicator.js
+++ packages/wix-style-react/src/AvatarGroup/moreIndicator/MoreIndicator.js
@@ -2,12 +2,10 @@ import React from 'react';
 import Avatar from '../../Avatar';
 import { dataHooks } from '../constants';
 
-const MoreItemAvatar = ({className, size, text}) => {
-  const key = `${className}${size}${text}`;
+const MoreIndicator = ({className, size, text}) => {
   return (
     <Avatar
       dataHook={dataHooks.avatarGroupMoreItem}
-      key={key}
       className={className}
       size={size}
       text={text}
@@ -15,4 +13,4 @@ const MoreItemAvatar = ({className, size, text}) => {
   );
 };
 
-export default MoreItemAvatar;
+export default MoreIndicator;
