d036a8fecf4bc98b1c15a34fbb2ddc0a0495ea08
roibWix <roib@wix.com>
2021-02-14 15:25:20 +0200

    update types

--- packages/wix-style-react/src/AvatarGroup/AvatarGroup.js
+++ packages/wix-style-react/src/AvatarGroup/AvatarGroup.js
@@ -89,7 +89,16 @@ AvatarGroup.propTypes = {
   /**
    * Use to pass an array of avatars
    */
-  items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  items: PropTypes.arrayOf(PropTypes.shape({
+    ariaLabel: PropTypes.string,
+    color: PropTypes.oneOf(['A1','A2','A3','A4','A5','A6']),
+    imgProp: PropTypes.object,
+    name: PropTypes.string,
+    text: PropTypes.string,
+    placeholder: PropTypes.node,
+    title: PropTypes.string,
+    dataHook: PropTypes.string
+  })).isRequired,
 };
 
 AvatarGroup.defaultProps = {
