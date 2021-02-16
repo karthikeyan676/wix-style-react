260cdd5a576b396263884fd50a658516d8373e0b
Jonathan Adler <jonathana@wix.com>
2021-02-16 16:55:54 +0200

    Add files of course

---
+++ packages/wix-ui-tpa/src/components/Pagination/perf/PaginationBasic.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { Pagination } from '../Pagination';
+
+export const PaginationExtended = () => {
+  return <Pagination totalPages={1} onChange={() => {}} />;
+};
