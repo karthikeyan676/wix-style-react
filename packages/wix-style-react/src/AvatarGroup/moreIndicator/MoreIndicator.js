772d8e31a0aa8085aba3c629af3821eab28b0aa0
roibWix <roib@wix.com>
2021-02-17 11:36:37 +0200

    revert

--- packages/wix-style-react/src/AvatarGroup/moreIndicator/MoreIndicator.js
+++ packages/wix-style-react/src/AvatarGroup/moreIndicator/MoreIndicator.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import Avatar from '../../Avatar';
-import {classes} from '../AvatarGroup.st.css';
 import {dataHooks} from '../constants';
 
 const MoreIndicator = ({className, size, text}) => {
   return (
-    <div className={classes.avatarContainer}>
       <Avatar
         dataHook={dataHooks.avatarGroupItem}
         data-indicator={dataHooks.avatarGroupMoreItem}
@@ -13,7 +11,6 @@ const MoreIndicator = ({className, size, text}) => {
         size={size}
         text={text}
       />
-    </div>
   );
 };
 
